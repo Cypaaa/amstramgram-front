9315df785acf854c03be634dc69715f6fb8dcdd8
Cypaaa <email@example.com>
2024-07-17 19:44:26 +0200

    fix profile page

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -4,12 +4,12 @@ const home = async (req, res) => {
     // get following list, get latest posts from following list
     // else (no follows)
     try {
-    const users = await userService.getUsers(req.cookies['token']);
-    if (!Array.isArray(users)) {
-        throw new Error("couldn't request user list");
-    }
+        const users = await userService.getUsers(req.cookies['token']);
+        if (!Array.isArray(users)) {
+            throw new Error("couldn't request user list");
+        }
 
-    res.render('pages/home.ejs', { users });
+        res.render('pages/home.ejs', { users });
     } catch (error) {
         res.status(500).json("Internal error");
     }
@@ -17,15 +17,24 @@ const home = async (req, res) => {
 
 const profile = async (req, res) => {
     try {
-    const user = await userService.getUser(req.cookies['token'], req.params.uuid);
-    if (user == null) {
-        throw new Error("couldn't request user list");
-    }
+        const user = await userService.getUser(req.cookies['token'], req.params.uuid);
+        if (user == null) {
+            throw new Error("couldn't request user list");
+        }
 
-    res.render('pages/user.ejs', { user });
+        res.render('pages/user.ejs', { user });
     } catch (error) {
         res.status(500).json("Internal error");
     }
+};
+
+const profileMe = async (req, res) => {
+    if (typeof(req.cookies['uuid']) != "string" || req.cookies['uuid'].length != 32) {
+        return res.redirect('/login');
+    }
+
+    req.params.uuid = req.cookies['uuid'];
+    profile(req, res);
 };
 
 const loginGet = async (req, res) => {
@@ -37,6 +46,7 @@ const loginPost = async (req, res) => {
     const result = await userService.Login(username_or_email, username_or_email, password);
     if (result?.token != null) {
         res.cookie('token', result.token);
+        res.cookie('uuid', result.user.uuid);
         res.redirect('/posts');
     } else {
         res.redirect('/login');
@@ -46,6 +56,7 @@ const loginPost = async (req, res) => {
 const postController = {
     home,
     profile,
+    profileMe,
     loginGet,
     loginPost
 };
--- routers/userRouter.mjs
+++ routers/userRouter.mjs
@@ -1,9 +1,11 @@
 import express from "express";
+import authMiddleware from "../middlewares/authMiddleware.mjs";
 import userController from "../controllers/userController.mjs";
 
 const userRouter = express.Router();
 
 userRouter.get('/', userController.home);
+userRouter.get('/users/me', authMiddleware.required, userController.profileMe);
 userRouter.get('/users/:uuid', userController.profile);
 userRouter.get('/login', userController.loginGet);
 userRouter.post('/login', userController.loginPost);
